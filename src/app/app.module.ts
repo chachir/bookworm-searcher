0a4380d55b440a13782c2073a0b70ca339aec284
Alejandro Leiva <alex-lp@hotmail.es>
2021-02-23 11:27:45 +0100

    Creación de los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
+import { SearchedComponent } from './searched/searched.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BookComponent,
+    SearchedComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searched/searched.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searched',
+  templateUrl: './searched.component.html',
+  styleUrls: ['./searched.component.css']
+})
+export class SearchedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
