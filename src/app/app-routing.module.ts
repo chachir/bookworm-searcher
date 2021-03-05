aad3261bd6ea5c32800905cba3df9a56deaf3766
Alejandro Leiva <guiot94@gmail.com>
2021-03-05 13:01:02 +0100

    Vista book y rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { HomeComponent } from './home/home.component';
+import { SearchedComponent } from './searched/searched.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'home', component: HomeComponent },
+  { path:'tienda', component: BookComponent },
+  { path:'contacto', component: SearchedComponent },
+  { path:'**', component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
