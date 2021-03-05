8bcf6421a6d9250caa604b63aa8379fa8b402347
nadia <nadcarcha@gmail.com>
2021-03-05 11:47:49 +0100

    category & subcategory interfaces added

--- src/app/searched/searched.component.ts
+++ src/app/searched/searched.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from '../category';
+import { Subcategory } from '../subcategory';
+
+import {APIService} from '../Servicios/api.service';
+
 @Component({
   selector: 'app-searched',
   templateUrl: './searched.component.html',
   styleUrls: ['./searched.component.css']
 })
 export class SearchedComponent implements OnInit {
-
-  constructor() { }
+  categories: Category[] = [];
+  subcategories: Subcategory[] = [];
+  
+  constructor(private APIService: APIService) { }
 
   ngOnInit(): void {
+  }
+
+  getCategories(): void{ //minutes
+    this.APIService.getAllCategories().subscribe((categories) => {this.categories = categories; });
   }
 
 }
---
+++ src/app/subcategory.ts
@@ -0,0 +1,5 @@
+export interface Subcategory {
+    subcategory_id: number;
+    name: String;
+    nicename: String;
+}
