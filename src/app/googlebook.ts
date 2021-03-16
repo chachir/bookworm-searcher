f5758e0b7b6c0ece31827ec4e094a7fbcfce4144
Alejandro Leiva <guiot94@gmail.com>
2021-03-16 12:41:01 +0100

    Interfaz googleBooks

--- src/app/googlebook.ts
+++ src/app/googlebook.ts
@@ -1,2 +1,29 @@
 export interface Googlebook {
+  saleInfo: {
+    country: string;
+    saleability: string;
+    isEbook: boolean;
+    buyLink: string
+  };
+  accesInfo:{
+    country: string;
+    viewability: string;
+    embeddable: boolean;
+    publicDomain: boolean;
+    textToSpeechPermission: string;
+    epub:{
+      isAvailable: boolean;
+      downloadLink: string
+    };
+    pdf:{
+      isAvailable: boolean;
+      downloadLink: string
+    };
+    webReaderLink: string;
+    accessViewStatus: string;
+    quoteSharingAllowed: boolean
+  };
+  searchInfo:{
+    textSnippet: string
+  };
 }
