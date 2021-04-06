59943a5f10a19874fb91c555cca68a23518c2939
nadia <nadcarcha@gmail.com>
2021-04-06 10:55:53 +0200

    fixes on numberPages

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { APIService } from '../services/api.service';
+import { Googlebook } from '../interfaces/googlebook';
 
 @Component({
   selector: 'app-book',
@@ -14,22 +15,26 @@ export class BookComponent implements OnInit {
   hours: number;
   minutes: number;
 
+  recentBooks: Googlebook;
+
   constructor(private APIService: APIService) { }
 
   ngOnInit(): void {
-    //this.getReadingSpeeding(604);
+    this.getReadingSpeeding(604);
   }
 
   /**to do - unresolved */
-  /*getReadingSpeeding(idBook: number): number{ //minutes
-    this.APIService.getBookByID(idBook).subscribe((book) => {this.book = book; this.numberPages = this.book.pop()?.pages; });
+  getReadingSpeeding(idBook: number): number{ //minutes
+    this.APIService.search('subject:' + "Language" + '&orderBy=newest').subscribe((recentBooks) => {this.recentBooks = recentBooks;
+                                                                                                    this.numberPages = this.recentBooks.items?.pop()?.volumeInfo.pageCount; });
     console.log(this.numberPages);
-    this.time = parseInt(this.numberPages!, 10) * this.averageWPM / this.averageWordsPage;
+    console.log(3);
+    /*this.time = Number(this.numberPages) * this.averageWPM / this.averageWordsPage;
     this.hours = this.time / 60;
     this.minutes = this.time % 60;
-    console.log(this.hours + '+' + this.minutes + '+' + this.time);
+    console.log(this.hours + '+' + this.minutes + '+' + this.time);*/
     return this.minutes;
-  }*/
+  }
 
 
 }
