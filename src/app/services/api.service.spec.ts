ea5470bfeaff1fd4cd8bfad7e958f218550aedbe
nadia <nadcarcha@gmail.com>
2021-04-16 11:53:24 +0200

    testing api service

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,30 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { APIService } from './api.service';
+import {HttpClientModule} from '@angular/common/http';
 
-describe('APIserviceService', () => {
-  let service: APIService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(APIService);
-  });
+describe('APIService', () => {
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  }); 
-});
+      beforeEach(() => TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule], 
+        providers: [APIService]
+      }));
+
+      it('should be created', () => {
+       const service: APIService = TestBed.inject(APIService);
+       expect(service).toBeTruthy();
+      });
+
+      it('should have search function', () => {
+       const service: APIService = TestBed.inject(APIService);
+       expect(service.search).toBeTruthy();
+      });
+      
+      it('should have searchById function', () => {
+       const service: APIService = TestBed.inject(APIService);
+       expect(service.searchById).toBeTruthy();
+      });
+
+    });
